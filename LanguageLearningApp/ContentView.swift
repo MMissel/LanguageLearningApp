e36be72ee2d41d0d48ec804f81d1b381972123c2
Marco2344 <mohammed.misselmany@gmail.com>
2024-05-01 19:42:14 +1000

    Initial Commit

---
+++ LanguageLearningApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LanguageLearningApp
+//
+//  Created by User on 30/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Japanese Memoriser!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LanguageLearningApp/LanguageLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageLearningAppApp.swift
+//  LanguageLearningApp
+//
+//  Created by User on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct LanguageLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
