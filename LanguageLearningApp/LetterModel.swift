22d21ae1b50dda2a479f40d10f657187506a7cda
Marco2344 <mohammed.misselmany@gmail.com>
2024-05-03 19:30:40 +1000

    Added a folder containing all the images for the japanese alphabet. Edited the list code to display a list of all the characters

--- LanguageLearningApp/LetterModel.swift
+++ LanguageLearningApp/LetterModel.swift
@@ -18,9 +18,9 @@ struct letter: Identifiable {
 //this is a list of the letters
 //we will need to store this list in a core data database
 struct letterList {
-    static let KatakanaAlphabet = [letter(imageName: "a", letterName: "a"),
-                                   letter(imageName: "e", letterName: "e"),
-                                   letter(imageName: "u", letterName: "u"),
-                                   letter(imageName: "i", letterName: "i"),
-                                   letter(imageName: "o", letterName: "o")]
+    static let KatakanaAlphabet = [letter(imageName: "icons8-katakana-a-100", letterName: "a"),
+                                   letter(imageName: "icons8-katakana-e-100", letterName: "e"),
+                                   letter(imageName: "icons8-katakana-u-100", letterName: "u"),
+                                   letter(imageName: "icons8-katakana-i-100", letterName: "i"),
+                                   letter(imageName: "icons8-katakana-a-100", letterName: "o")]
 }
